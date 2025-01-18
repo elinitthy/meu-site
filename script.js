7376225d619b36b99dcaebf65abdb083222a6021
elinitthy <chittandaeru5401@gmail.com>
2025-01-18 10:59:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Hello my lover! ✨")
+
+let btn = document.querySelector('#meuBotao');
+if (btn) {
+    btn.addEventListener('click', function () {
+        console.log("Botão clicado!");
+        alert("feliz primeiro dia com 23 anos! já agradeceu a Deus por mais um ano de vida? sou grata por você na minha vida! ps: você diz sim e eu digo não🌷🩷");
+    });
+} else {
+    console.log("Botão não encontrado.");
+}
